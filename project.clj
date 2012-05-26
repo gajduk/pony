b6245fde623c118a403697e051c67b029b858166
Nimalan Mahendran <nmahendran@alumni.uwaterloo.ca>
2012-05-26 12:44:28 -0700

    Adding Maven install to local repository for ParallelColt

--- project.clj
+++ project.clj
@@ -1,3 +1,4 @@
-(defproject pony "1.0.0-SNAPSHOT"
+(defproject pony "0.1.0"
   :description "FIXME: write description"
-  :dependencies [[org.clojure/clojure "1.3.0"]])
+  :dependencies [[org.clojure/clojure "1.3.0"]
+                 [pony/parallelcolt "0.9.4"]])
