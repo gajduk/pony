ec79398f0eaa77042ea7a9b3d8a3e33234a6ec96
Nimalan Mahendran <nmahendran@alumni.uwaterloo.ca>
2012-05-26 12:13:16 -0700

    Adding initial Leiningen-generated project files

---
+++ src/pony/core.clj
@@ -0,0 +1,1 @@
+(ns pony.core)
---
+++ test/pony/test/core.clj
@@ -0,0 +1,6 @@
+(ns pony.test.core
+  (:use [pony.core])
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
