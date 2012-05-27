6c968731984a567973389e029fbe006f23a94fc2
Nimalan Mahendran <nmahendran@alumni.uwaterloo.ca>
2012-05-26 17:50:58 -0700

    Adding some functions, some macros and Marginalia docs

--- project.clj
+++ project.clj
@@ -1,4 +1,8 @@
 (defproject pony "0.1.0"
   :description "FIXME: write description"
+  :repl-options [:init nil :caught clj-stacktrace.repl/pst+]
+  :dev-dependencies [[org.clojure/clojure "1.3.0" :classifier "sources"]
+                     [org.clojure/tools.trace "0.7.1"]
+                     [lein-marginalia "0.7.0"]]
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [pony/parallelcolt "0.9.4"]])
--- src/pony/core.clj
+++ src/pony/core.clj
@@ -1,1 +1,72 @@
-(ns pony.core)
+;; ## A thin wrapper around ParallelColt
+;; Pony is a wrapper around the ParallelColt library
+;; that is intended to be as close as possible to the original library
+;; (to allow full usage of it) while being as close to Clojure as possible.
+
+(ns pony.core
+  (:import [cern.colt.matrix AbstractMatrix AbstractMatrix2D]
+           [cern.colt.matrix.tdouble DoubleMatrix2D]
+           [cern.colt.matrix.tdouble.impl DenseDoubleMatrix2D]
+           [cern.colt.matrix.tfloat FloatMatrix2D]
+           [cern.colt.matrix.tfloat.impl DenseFloatMatrix2D]))
+
+(defn zeros [^long rows ^long cols]
+  (DenseDoubleMatrix2D. rows cols))
+
+;; ## Reconciling the difference between a mutable Java library and an
+;; ## immutable Clojure style?
+;; How exactly can a Clojure-like wrapper be built when ParallelColt
+;; matrices are made up of operations that are meant to destructively
+;; modify the operand? Ideally, the matrix would be implemented as a
+;; persistent collection with transient support for performing a chain
+;; of operations. A Clojure group thread called "How to add a new
+;; type of collection?" talks about why it is not possible to do this
+;; in Clojure right now.
+
+;; These are just thin wrappers around ParallelColt functions.
+(defn copy [^AbstractMatrix m] (.copy m))
+
+(defn arg-matmin [^AbstractMatrix m]
+  (let [[min & coords] (.getMinLocation m)]
+    [min (map long coords)]))
+(defn matmin [^AbstractMatrix m]
+  (first (.getMinLocation m)))
+
+(defn arg-matmax [^AbstractMatrix m]
+  (let [[max & coords] (.getMaxLocation m)]
+    [max (map long coords)]))
+(defn matmax [^AbstractMatrix m]
+  (first (.getMaxLocation m)))
+
+(defn trans [^AbstractMatrix2D m]
+  (.viewDice m))
+
+;;  These macros copy the ParallelColt matrix m, do the equivalent of
+;;  -> or ->> with it and return the result. They try to approximate
+;;  the (do (transient) (do ops) (persistent!)) semantics of transient
+;;  collections.
+(defmacro mat->
+  [^AbstractMatrix2D m ops]
+  (let [copy-m (gensym)]
+    `(let [~copy-m (.copy ~m)]
+       (-> ~copy-m ~@ops))))
+(defmacro mat->>
+  [^AbstractMatrix2D m ops]
+  (let [copy-m (gensym)]
+    `(let [~copy-m (.copy ~m)]
+       (->> ~copy-m ~@ops))))
+
+;; These macros are equivalent to the ones above, except no copy is
+;; done and hence they are not safe.
+(defmacro mat->!
+  [^AbstractMatrix2D m ops]
+  `(-> ~m ~@ops))
+(defmacro mat->>!
+  [^AbstractMatrix2D m ops]
+  `(->> ~m ~@ops))
+
+;; ## Being conscious of speed
+;; Reflection adds overhead that we don't really need, since we
+;; already know the types of the matrix elements. We can get quite far
+;; by avoiding reflection with typehints and simple functions.
+(set! *warn-on-reflection* true)
