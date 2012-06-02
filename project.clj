e9e2c5a19baf6dace81ee2ccbcc75ec0a109c9e6
Nimalan Mahendran <nmahendran@alumni.uwaterloo.ca>
2012-06-02 00:00:46 -0700

    Adding local Maven repository

--- project.clj
+++ project.clj
@@ -5,4 +5,5 @@
                      [org.clojure/tools.trace "0.7.1"]
                      [lein-marginalia "0.7.0"]]
   :dependencies [[org.clojure/clojure "1.3.0"]
-                 [pony/parallelcolt "0.9.4"]])
+                 [pony/parallelcolt "0.9.4"]]
+  :repositories {"releases" ~(str (.toURI (java.io.File. "~/.m2/repository")))})
