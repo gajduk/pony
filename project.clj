96158cb4c3ec945c80aa891a7de0f1bc700b31e8
Nimalan Mahendran <nimalan.mahendran@gmail.com>
2012-09-01 15:22:04 -0700

    Upgraded project.clj to Leiningen 2

--- project.clj
+++ project.clj
@@ -1,9 +1,6 @@
 (defproject pony "0.1.0"
   :description "FIXME: write description"
-  :repl-options [:init nil :caught clj-stacktrace.repl/pst+]
-  :dev-dependencies [[org.clojure/clojure "1.3.0" :classifier "sources"]
-                     [org.clojure/tools.trace "0.7.1"]
-                     [lein-marginalia "0.7.0"]]
-  :dependencies [[org.clojure/clojure "1.3.0"]
+  :dependencies [[org.clojure/clojure "1.4.0"]
+                 [org.clojure/tools.nrepl "0.2.0-beta9"]
                  [pony/parallelcolt "0.9.4"]]
   :repositories {"releases" ~(str (.toURI (java.io.File. "~/.m2/repository")))})
