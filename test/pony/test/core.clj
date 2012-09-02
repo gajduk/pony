546be2b1b7b8293ca407a05044873d682a024729
Nimalan Mahendran <nimalan.mahendran@gmail.com>
2012-09-01 22:42:22 -0700

    Adding unit test for assignment

--- test/pony/test/core.clj
+++ test/pony/test/core.clj
@@ -2,5 +2,14 @@
   (:use [pony.core])
   (:use [clojure.test]))
 
-(deftest replace-me ;; FIXME: write
-  (is false "No tests have been written."))
+(deftest assign-test
+  (let [m (make-matrix 4 3)
+        assigned-m
+        (-> m
+            transient
+            (assign 1.0)
+            persistent!)
+        matrix (. assigned-m m)]
+    (is
+     (every? #(= %1 1.0)
+             (for [r (range 4) c (range 3)] (.get matrix r c))))))
